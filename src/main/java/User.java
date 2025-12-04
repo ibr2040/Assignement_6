66064d178425f0c9d66aa990d79d8d329e2f3850
MuslleMummy <glebanator2000pro@gmail.com>
2025-12-04 20:41:27 +0100

    Added documentation and basic classes

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -36,7 +36,6 @@ abstract class User implements Serializable{
         if (email==null ||email.isBlank()){
             throw new IllegalArgumentException("Email cannot be empty");
         }
-
         if (!email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")){
             throw new IllegalArgumentException("Invalid email format");
         }
